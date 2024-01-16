b3d3a45a62b7ef224e4e7587a67260ea8ebd51c2
alenasmartp <150282056+alenasmartp@users.noreply.github.com>
2024-01-16 16:56:03 +0300

    Add files via upload

---
+++ TelRun_10_24/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int box1 = 7;
+                System.out.println(box1);
+
+                byte box2 = 70;
+        System.out.println(box2);
+
+        short box3 = 77;
+        System.out.println(box3);
+
+        long box4 = 7777;
+        System.out.println(box4);
+
+        double box5 = 7.77;
+        System.out.println(box5);
+
+        String box6 = "Tekst";
+        System.out.println(box6);
+
+        char box7 = '@';
+        System.out.println(box7);
+
+
+    }
+}
